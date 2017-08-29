ad7b73a5743f9ee8b7564fc9a630fd592a5f8a93
himani-webonise <himani.sharma@weboniselab.com>
2017-08-29 16:36:53 +0530

    Converted Clock function to react class

--- src/index.js
+++ src/index.js
@@ -5,23 +5,22 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-function Clock(props) {
-  
-  return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {props.date.toLocaleTimeString()}.</h2>
-      </div>
-    )
+class Clock extends React.Component {
+    render() {
+        return (
+          <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+          </div>
+        )
+    }
 }
 
-var date 
-
 function tick() {
-  ReactDOM.render(
-    <Clock date={new Date()}/>,
-    document.getElementById('root')
-  );
+    ReactDOM.render(
+        <Clock date={new Date()}/>,
+        document.getElementById('root')
+    );
 }
 
 
