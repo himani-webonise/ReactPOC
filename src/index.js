1cc3be8ce31327deaa050e0e2452888fd5658003
himani-webonise <himani.sharma@weboniselab.com>
2017-08-29 16:41:01 +0530

    Defined state:Add local State to a Class

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
     render() {
         return (
           <div>
             <h1>Hello, world!</h1>
-            <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
           </div>
         )
     }
@@ -18,7 +23,7 @@ class Clock extends React.Component {
 
 function tick() {
     ReactDOM.render(
-        <Clock date={new Date()}/>,
+        <Clock/>,
         document.getElementById('root')
     );
 }
