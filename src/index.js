c25858efc0aa2ad28a1fa1241564ef5f8ca04c98
himani-webonise <himani.sharma@weboniselab.com>
2017-08-29 16:32:25 +0530

    Basic component and props without lifecycle

--- src/index.js
+++ src/index.js
@@ -4,5 +4,26 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+function Clock(props) {
+  
+  return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {props.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
+}
+
+var date 
+
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()}/>,
+    document.getElementById('root')
+  );
+}
+
+
+setInterval(tick, 1000);
 registerServiceWorker();
