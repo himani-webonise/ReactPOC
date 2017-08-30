81cd1a0316f7ac7b98dd9db97402591f55d7a267
himani-webonise <himani.sharma@weboniselab.com>
2017-08-30 17:14:06 +0530

    update header user name by the value entered in the text field on click of the 'Update User' button

--- src/App.js
+++ src/App.js
@@ -3,19 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            name : 'user'
+        }
+        this.updateUser = this.updateUser.bind(this)
+    }
+
+    updateUser() {
+        let userInput = document.getElementById('userInput')
+        this.setState({
+            name: userInput.value
+        })
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div className="App-header">
+                    <Header name={this.state.name}/>
+                </div>
+                <div>
+                    <input id="userInput" type="text"/>
+                    <button onClick={this.updateUser}>Update User</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+function Header(props) {
+    return (<h2>Welcome to React {props.name}</h2>)
 }
 
 export default App;
