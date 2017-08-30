fe711454902a0f42fc8587625fa35c69b8ef95cc
himani-webonise <himani.sharma@weboniselab.com>
2017-08-30 12:43:13 +0530

    onClick event handling using toggle component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Toggle from './Toggle';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Toggle />, document.getElementById('root'));
+
 registerServiceWorker();
