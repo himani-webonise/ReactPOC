3cd53b86015b1aace6b74be9dfeee957a2681bd1
himani-webonise <himani.sharma@weboniselab.com>
2017-08-31 19:10:05 +0530

    Student statics home layout init

--- src/App.js
+++ src/App.js
@@ -3,19 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    constructor(props) {
+        super(props)
+        this.state= {
+            isDistinction: false,
+            isFirstCls: false,
+            isSecondCls: false,
+            isFail: false
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this)
+    }
+
+    handleInputChange(event) {
+        console.log(event)
+        const name = event.target.name
+        this.setState({
+            [name]: event.target.checked
+        })
+    }
+
+    render() {
+      return (
+        <div className="App">
+            <div className="App-header">
+                <h2>Student Statics</h2>
+            </div>
+            <div className="body">
+                <div className="searchCnt">
+                    <input className="searchBox" type="text"/>
+                    <button className="searchBtn">Search</button>
+                </div>
+                <div className="selectionCnt">
+                    Ditinction <input name="isDistinction" type="checkbox" checked={this.state.isDistinction} onChange={this.handleInputChange}/>
+                    First Class <input name="isFirstCls" type="checkbox"checked={this.state.isFirstCls} onChange={this.handleInputChange}/>
+                    Second Class <input name="isSecondCls" type="checkbox"checked={this.state.isSecondCls} onChange={this.handleInputChange}/>
+                    Fail <input name="isFail" type="checkbox" checked={this.state.isFail} onChange={this.handleInputChange}/>
+                </div>
+                <div className="studentRecordCnt"></div>
+            </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      );
+    }
 }
 
 export default App;
