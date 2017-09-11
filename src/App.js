c8a072aedaa270362c3768f186c204e4984c59f9
himani-webonise <himani.sharma@weboniselab.com>
2017-09-11 16:57:57 +0530

    Add result.json file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import studentRecords from './results.json';
 
 class App extends Component {
     constructor(props) {
@@ -13,6 +14,7 @@ class App extends Component {
         }
 
         this.handleInputChange = this.handleInputChange.bind(this)
+        console.log(studentRecords)
     }
 
     handleInputChange(event) {
@@ -40,7 +42,40 @@ class App extends Component {
                     Second Class <input name="isSecondCls" type="checkbox"checked={this.state.isSecondCls} onChange={this.handleInputChange}/>
                     Fail <input name="isFail" type="checkbox" checked={this.state.isFail} onChange={this.handleInputChange}/>
                 </div>
-                <div className="studentRecordCnt"></div>
+                <div className="studentRecordCnt">
+                    <table>
+                        <tr>
+                          <th>First Name</th>
+                          <th>Last Name</th>
+                          <th>Percentage</th>
+                        </tr>
+                        <tr>
+                          <td>Alfreds Futterkiste</td>
+                          <td>Maria Anders</td>
+                          <td>Germany</td>
+                        </tr>
+                        <tr>
+                          <td>Centro comercial Moctezuma</td>
+                          <td>Francisco Chang</td>
+                          <td>Mexico</td>
+                        </tr>
+                        <tr>
+                          <td>Ernst Handel</td>
+                          <td>Roland Mendel</td>
+                          <td>Austria</td>
+                        </tr>
+                        <tr>
+                          <td>Island Trading</td>
+                          <td>Helen Bennett</td>
+                          <td>UK</td>
+                        </tr>
+                        <tr>
+                          <td>Laughing Bacchus Winecellars</td>
+                          <td>Yoshi Tannamuri</td>
+                          <td>Canada</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
       );
